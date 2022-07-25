2d4ac5fc4de4e52167139516a72c05c54d2cec49
Suryansh148 <suryanshsinghchauhan148@gmail.com>
2022-07-26 00:59:05 +0530

    second commit

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ app.get('/:shortUrl',async (req,res)=>{
     shortUrl.save();
     res.redirect(shortUrl.full);
 });
-app.listen(5000,()=>{
-    console.log('server started at 5000');
+app.listen(process.env.PORT||5000,()=>{
+    console.log('server started.....');
 });
